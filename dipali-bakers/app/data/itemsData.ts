94d0abfc9760d3e82b7aa4f63d4db820d1aaaef2
Anish59312 <anish31442@gmail.com>
2025-03-01 01:03:00 +0550

    Update menu items from Google Sheets

--- dipali-bakers/app/data/itemsData.ts
+++ dipali-bakers/app/data/itemsData.ts
@@ -1,69 +1,598 @@
 export const menuSections = [
   {
+    "id": "fix-thali",
+    "title": "Fix Thali"
+  },
+  {
+    "id": "cold-and-juice",
+    "title": "Cold and Juice"
+  },
+  {
     "id": "breakfast",
     "title": "Breakfast"
   },
   {
-    "id": "punjabi-veg.",
-    "title": "Punjabi Veg."
+    "id": "south-indian",
+    "title": "South Indian"
   },
   {
     "id": "chinese",
     "title": "Chinese"
   },
   {
-    "id": "beverages",
-    "title": "Beverages"
+    "id": "punjabi-veg",
+    "title": "Punjabi Veg"
+  },
+  {
+    "id": "sandwitches",
+    "title": "Sandwitches"
+  },
+  {
+    "id": "chatt",
+    "title": "Chatt"
   }
 ]
 
 export const menuItems = {
-  "breakfast": [
+  "fix-thali": [
     {
       "id": 1,
-      "name": "Samosa",
-      "price": 30
+      "name": "Chole Bhature",
+      "price": 50
     },
     {
       "id": 2,
-      "name": "Mendu Vada",
-      "price": 30
+      "name": "Aloo Paratha",
+      "price": 40
     },
     {
       "id": 3,
-      "name": "Poha",
-      "price": 15
-    }
-  ],
-  "punjabi-veg.": [
+      "name": "Punjabi Thali Fix",
+      "price": 80
+    },
     {
       "id": 4,
-      "name": "Hydrabadi Biryani",
-      "price": 65
+      "name": "Punjabi Thali Fix Half",
+      "price": 40
     },
     {
       "id": 5,
-      "name": "Veg. Biryani",
-      "price": 250
+      "name": "Gujarati Thali Fix ",
+      "price": 70
+    },
+    {
+      "id": 6,
+      "name": "Gujarati Thali Fix Half",
+      "price": 35
+    },
+    {
+      "id": 7,
+      "name": "Extra Roti",
+      "price": 5
+    }
+  ],
+  "cold-and-juice": [
+    {
+      "id": 8,
+      "name": "Lassi",
+      "price": 15
+    },
+    {
+      "id": 9,
+      "name": "Cold Coffee",
+      "price": 40
+    },
+    {
+      "id": 10,
+      "name": "Butter Milk",
+      "price": 10
+    },
+    {
+      "id": 11,
+      "name": "Jal Jeera",
+      "price": 20
+    }
+  ],
+  "breakfast": [
+    {
+      "id": 12,
+      "name": "Tea",
+      "price": 10
+    },
+    {
+      "id": 13,
+      "name": "Coffee",
+      "price": 20
+    },
+    {
+      "id": 14,
+      "name": "Poha",
+      "price": 15
+    },
+    {
+      "id": 15,
+      "name": "Khaman",
+      "price": 20
+    },
+    {
+      "id": 16,
+      "name": "Sev Dhokla",
+      "price": 20
+    },
+    {
+      "id": 17,
+      "name": "Samosa",
+      "price": 20
+    },
+    {
+      "id": 18,
+      "name": "Kachori",
+      "price": 20
+    },
+    {
+      "id": 19,
+      "name": "Aloo Vada",
+      "price": 15
+    },
+    {
+      "id": 20,
+      "name": "Bread Pakoda",
+      "price": 20
+    },
+    {
+      "id": 21,
+      "name": "Bread Roll",
+      "price": 15
+    },
+    {
+      "id": 22,
+      "name": "Cheese Roll",
+      "price": 30
+    },
+    {
+      "id": 23,
+      "name": "Methi Gota",
+      "price": 25
+    },
+    {
+      "id": 24,
+      "name": "Dal Vada",
+      "price": 25
+    }
+  ],
+  "south-indian": [
+    {
+      "id": 25,
+      "name": "Upma",
+      "price": 40
+    },
+    {
+      "id": 26,
+      "name": "Masala Uttapam",
+      "price": 50
+    },
+    {
+      "id": 27,
+      "name": "Tomato Uttapam",
+      "price": 50
+    },
+    {
+      "id": 28,
+      "name": "Onian Uttapam",
+      "price": 50
+    },
+    {
+      "id": 29,
+      "name": "Idli Sambhar",
+      "price": 30
+    },
+    {
+      "id": 30,
+      "name": "Methi Vada",
+      "price": 30
+    },
+    {
+      "id": 31,
+      "name": "Plain Dhosa",
+      "price": 40
+    },
+    {
+      "id": 32,
+      "name": "Masala Dhosa",
+      "price": 50
+    },
+    {
+      "id": 33,
+      "name": "Cheese Plain Dhosa",
+      "price": 50
+    },
+    {
+      "id": 34,
+      "name": "Cheese Masala Dhosa",
+      "price": 70
+    },
+    {
+      "id": 35,
+      "name": "Mysore Plain Dhosa",
+      "price": 50
+    },
+    {
+      "id": 36,
+      "name": "Mysore Masala Dhosa",
+      "price": 60
+    },
+    {
+      "id": 37,
+      "name": "Mysore Masala Cheese Dhosa",
+      "price": 80
     }
   ],
   "chinese": [
     {
-      "id": 6,
-      "name": "Chinese Bhel",
+      "id": 38,
+      "name": "Manchurian Dry Half",
+      "price": 50
+    },
+    {
+      "id": 39,
+      "name": "Manchurian Dry Full",
+      "price": 80
+    },
+    {
+      "id": 40,
+      "name": "Manchurian Gravy Half",
+      "price": 50
+    },
+    {
+      "id": 41,
+      "name": "Manchurian Gravy Full",
+      "price": 80
+    },
+    {
+      "id": 42,
+      "name": "Manchurian Noodles Half",
+      "price": 60
+    },
+    {
+      "id": 43,
+      "name": "Manchurian Noodles Full",
+      "price": 80
+    },
+    {
+      "id": 44,
+      "name": "Manchurian Fried Rice Half",
+      "price": 50
+    },
+    {
+      "id": 45,
+      "name": "Manchurian Fried Rice Full",
+      "price": 80
+    },
+    {
+      "id": 46,
+      "name": "Fried Rice Half",
+      "price": 50
+    },
+    {
+      "id": 47,
+      "name": "Fried Rice Full",
+      "price": 80
+    },
+    {
+      "id": 48,
+      "name": "Noodles Half",
+      "price": 60
+    },
+    {
+      "id": 49,
+      "name": "Noodles Full",
+      "price": 80
+    },
+    {
+      "id": 50,
+      "name": "Panner Chilly Half",
       "price": 100
+    },
+    {
+      "id": 51,
+      "name": "Panner Chilly Full",
+      "price": 180
+    },
+    {
+      "id": 52,
+      "name": "Chinese Bhel Half",
+      "price": 60
+    },
+    {
+      "id": 53,
+      "name": "Chinese Bhel Full",
+      "price": 90
     }
   ],
-  "beverages": [
+  "punjabi-veg": [
     {
-      "id": 7,
-      "name": "Tea",
+      "id": 54,
+      "name": "Aloo Mutter",
       "price": 70
     },
     {
-      "id": 8,
-      "name": "Coffee",
+      "id": 55,
+      "name": "Jeera Aloo",
+      "price": 70
+    },
+    {
+      "id": 56,
+      "name": "Chana Masala",
+      "price": 70
+    },
+    {
+      "id": 57,
+      "name": "Sev Tomato",
+      "price": 70
+    },
+    {
+      "id": 58,
+      "name": "Mix Veg",
+      "price": 75
+    },
+    {
+      "id": 59,
+      "name": "Veg Kolhapuri",
+      "price": 75
+    },
+    {
+      "id": 60,
+      "name": "Veg Kadai",
+      "price": 75
+    },
+    {
+      "id": 61,
+      "name": "Veg Jajpuri",
+      "price": 75
+    },
+    {
+      "id": 62,
+      "name": "Veg Singapuri",
+      "price": 75
+    },
+    {
+      "id": 63,
+      "name": "Malai Kofta",
+      "price": 75
+    },
+    {
+      "id": 64,
+      "name": "Cheese Kofta",
+      "price": 95
+    },
+    {
+      "id": 65,
+      "name": "Cheese Butter M/S",
+      "price": 95
+    },
+    {
+      "id": 66,
+      "name": "Paneer Butter M/S",
+      "price": 85
+    },
+    {
+      "id": 67,
+      "name": "Paneer Tikka M/S",
+      "price": 85
+    },
+    {
+      "id": 68,
+      "name": "Paneer Kadai",
+      "price": 85
+    },
+    {
+      "id": 69,
+      "name": "Paneer Bhurji",
+      "price": 95
+    },
+    {
+      "id": 70,
+      "name": "Paneer Handi",
+      "price": 85
+    },
+    {
+      "id": 71,
+      "name": "Paneer Tava M/S",
+      "price": 85
+    },
+    {
+      "id": 72,
+      "name": "Dal Fry",
+      "price": 55
+    },
+    {
+      "id": 73,
+      "name": "Dal Tadka",
+      "price": 60
+    },
+    {
+      "id": 74,
+      "name": "Jeera Rice",
+      "price": 60
+    },
+    {
+      "id": 75,
+      "name": "Veg. Biryani",
+      "price": 65
+    },
+    {
+      "id": 76,
+      "name": "Hyderabadi Biryani",
+      "price": 65
+    },
+    {
+      "id": 77,
+      "name": "Veg. Pulav",
+      "price": 60
+    }
+  ],
+  "sandwitches": [
+    {
+      "id": 78,
+      "name": "Bread Butter",
+      "price": 20
+    },
+    {
+      "id": 79,
+      "name": "Bread Butter Jam",
+      "price": 20
+    },
+    {
+      "id": 80,
+      "name": "Veg Sandwiches",
+      "price": 30
+    },
+    {
+      "id": 81,
+      "name": "Veg Grill S/W",
       "price": 40
+    },
+    {
+      "id": 82,
+      "name": "Aloo Mutter S/W",
+      "price": 30
+    },
+    {
+      "id": 83,
+      "name": "Cheese S/W",
+      "price": 30
+    },
+    {
+      "id": 84,
+      "name": "Cheese Grill S/W",
+      "price": 40
+    },
+    {
+      "id": 85,
+      "name": "Cheese Jam S/W",
+      "price": 40
+    },
+    {
+      "id": 86,
+      "name": "Veg Cheese S/W",
+      "price": 40
+    },
+    {
+      "id": 87,
+      "name": "Veg Cheese Grill S/W",
+      "price": 50
+    },
+    {
+      "id": 88,
+      "name": "Samosa Bun",
+      "price": 40
+    },
+    {
+      "id": 89,
+      "name": "Pizza",
+      "price": 50
+    },
+    {
+      "id": 90,
+      "name": "Italian Pizza",
+      "price": 70
+    },
+    {
+      "id": 91,
+      "name": "Hot Dog",
+      "price": 40
+    },
+    {
+      "id": 92,
+      "name": "Burger",
+      "price": 50
+    }
+  ],
+  "chatt": [
+    {
+      "id": 93,
+      "name": "Bhaji Pav",
+      "price": 40
+    },
+    {
+      "id": 94,
+      "name": "Butter Bhajpav",
+      "price": 60
+    },
+    {
+      "id": 95,
+      "name": "Vada Pav",
+      "price": 20
+    },
+    {
+      "id": 96,
+      "name": "Butter Vada Pav",
+      "price": 25
+    },
+    {
+      "id": 97,
+      "name": "Masala Pav",
+      "price": 60
+    },
+    {
+      "id": 98,
+      "name": "Dabeli",
+      "price": 20
+    },
+    {
+      "id": 99,
+      "name": "Butter Dabeli",
+      "price": 25
+    },
+    {
+      "id": 100,
+      "name": "Bhel",
+      "price": 30
+    },
+    {
+      "id": 101,
+      "name": "Dahi Vada",
+      "price": 30
+    },
+    {
+      "id": 102,
+      "name": "Aloo Chat",
+      "price": 30
+    },
+    {
+      "id": 103,
+      "name": "Chana Chaat",
+      "price": 25
+    },
+    {
+      "id": 104,
+      "name": "Samosa Chaat",
+      "price": 30
+    },
+    {
+      "id": 105,
+      "name": "Papdi Chaat",
+      "price": 40
+    },
+    {
+      "id": 106,
+      "name": "Plain Maggi",
+      "price": 30
+    },
+    {
+      "id": 107,
+      "name": "Masala Maggi",
+      "price": 40
+    },
+    {
+      "id": 108,
+      "name": "Veg Maggi",
+      "price": 40
+    },
+    {
+      "id": 109,
+      "name": "Cheese Maggi",
+      "price": 50
     }
   ]
 }
